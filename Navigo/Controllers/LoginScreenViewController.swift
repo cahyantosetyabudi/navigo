d0239b2b813d07c9e7dfb3c363b533d372f2fccc
Cahyanto Setya Budi <executionzee@gmail.com>
2018-12-08 22:00:10 +0700

    add layout login

---
+++ Navigo/Controllers/LoginScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SplashScreenViewController.swift
+//  Navigo
+//
+//  Created by Cahyanto Setya Budi on 12/8/18.
+//  Copyright © 2018 Cahyanto Setya Budi. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Navigo/SupportFiles/Designable/CustomSegmentedControl.swift
@@ -0,0 +1,119 @@
+//
+//  CustomSegmentedControl.swift
+//  Navigo
+//
+//  Created by Cahyanto Setya Budi on 12/8/18.
+//  Copyright © 2018 Cahyanto Setya Budi. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class CustomSegmentedControl: UIControl {
+    var buttons = [UIButton]()
+    var selector: UIView!
+    var selectedSegmentIndex = 0
+    
+    @IBInspectable var buttonTitleSeparatedByComma: String = "" {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = .clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float = 0 {
+        didSet {
+            layer.shadowOpacity = shadowOpacity
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable var textColor: UIColor = .lightGray {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var selectorColor: UIColor = .darkGray {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var selectorTextColor: UIColor = .black {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        buttons.removeAll()
+        subviews.forEach { (view) in
+            view.removeFromSuperview()
+        }
+        
+        let buttonTitles = buttonTitleSeparatedByComma.components(separatedBy: ",")
+        for buttonTitle in buttonTitles {
+            let button = UIButton(type: .system)
+            button.setTitle(buttonTitle, for: .normal)
+            button.setTitleColor(textColor, for: .normal)
+            button.titleLabel?.font = UIFont(name: "Gibson-Regular", size: 15)
+            button.addTarget(self, action: #selector(buttonTapped(button:)), for: .touchUpInside)
+            buttons.append(button)
+        }
+        
+        buttons[0].setTitleColor(selectorTextColor, for: .normal)
+        
+        let selectorWidth = self.layer.frame.size.width / CGFloat(2)
+        selector = UIView(frame: CGRect(x: 0, y: 41, width: selectorWidth, height: 4.0))
+        selector.backgroundColor = selectorColor
+        addSubview(selector)
+        
+        let stackView = UIStackView(arrangedSubviews: buttons)
+        stackView.alignment = .fill
+        stackView.axis = .horizontal
+        stackView.distribution = .fillProportionally
+        addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        stackView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        stackView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+    }
+    
+    override func draw(_ rect: CGRect) {
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+    }
+    
+    @objc func buttonTapped(button: UIButton) {
+        for (indexButton, btn) in buttons.enumerated() {
+            btn.setTitleColor(textColor, for: .normal)
+            
+            if btn == button {
+                selectedSegmentIndex = indexButton
+                let selectorStartPosition = frame.width / CGFloat(buttons.count) * CGFloat(indexButton)
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.selector.frame.origin.x = selectorStartPosition
+                })
+                btn.setTitleColor(selectorTextColor, for: .normal)
+            }
+        }
+        sendActions(for: .valueChanged)
+    }
+    
+}
